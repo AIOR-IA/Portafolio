8e3223977a520d1ca475ce138a6a5995f013a6f4
Lenar <lenar.toledo@gmail.com>
2022-08-29 21:29:19 -0400

    añadí la sección de About y ya casi termino la Sección de Skills, tambien añadi el effecto text-sphere

---
+++ js/TagSphere.js
@@ -0,0 +1,29 @@
+const Texts = [
+    'Html', 'Css', 'Javascript',
+    'Git', 'Bootstrap', 'Angular',
+    'Sass', 'Firebase',
+    'Photoshop',
+    'Sql Server', 'Mysql', 'TypeScript', 'C#'
+];
+
+var tagCloud = TagCloud('.Sphere', Texts, {
+
+    // Sphere radius in px
+    radius: 350,
+
+    // animation speed
+    // slow, normal, fast
+    maxSpeed: 'fast',
+    initSpeed: 'fast',
+
+    // Rolling direction [0 (top) , 90 (left), 135 (right-bottom)] 
+    direction: 135,
+
+    // interaction with mouse or not [Default true (decelerate to rolling init speed, and keep rolling with mouse).]
+    keep: true
+
+});
+
+// Giving color to each text in sphere
+var color = '#47E591';
+document.querySelector('.Sphere').style.color = color;
