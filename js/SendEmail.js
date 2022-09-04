9ff3570df2d64d0f4df389d3c692c75c481783bd
Lenar <lenar.toledo@gmail.com>
2022-09-03 21:07:20 -0400

    Termine de hacer revisar los inputs del form, y configuración del envio de email SMTP, ya solo falta añañadir los links de mis redes de github y termino el proyecto

--- js/SendEmail.js
+++ js/SendEmail.js
@@ -6,41 +6,97 @@ const message = document.getElementById('message');
 document.getElementById('form')
  .addEventListener('submit', function(event) {
    event.preventDefault();
-  
-   
-   const serviceID = 'default_service';
-   const templateID = 'template_o6dhata';
- 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      
-      
-      names.value='';
-      email.value='';
-      subject.value='';
-      message.value='';
-      Swal.fire(
-        {
-          title:'SUCCESS',
-          text:'Your message has been sent',
-          icon:'success',
-          background:'rgba(30,30,30,1)',
-          color:'#fff',
-        }
-      )
-    }, (err) => {
-      
-      email.value='';
-      subject.value='';
-      message.value='';
-      Swal.fire(
+   let textname = names.value;
+   let textemail = email.value;
+   let textsubject = subject.value;
+   let textmessage = message.value;
+   if(textname.trim().length <= 0){
+         Swal.fire(
         {
           title:'ERROR',
-          text:'Error: ' + err,
+          text:'Empty field in NAME'  ,
           icon:'error',
           background:'rgba(30,30,30,1)',
           color:'#fff',
         }
       )
-    });
+   }
+   else{
+     if(textemail.trim().length <= 0){
+      Swal.fire(
+        {
+          title:'ERROR',
+          text:'Empty field in EMAIL'  ,
+          icon:'error',
+          background:'rgba(30,30,30,1)',
+          color:'#fff',
+        }
+      )
+     }
+     else{
+        if(textsubject.trim().length <= 0){
+          Swal.fire(
+            {
+              title:'ERROR',
+              text:'Empty field in SUBJECT' ,
+              icon:'error',
+              background:'rgba(30,30,30,1)',
+              color:'#fff',
+            }
+          )
+        }
+        else{
+          if(textmessage.trim().length <= 0){
+            Swal.fire(
+              {
+                title:'ERROR',
+                text:'Empty field in MESSAGE' ,
+                icon:'error',
+                background:'rgba(30,30,30,1)',
+                color:'#fff',
+              }
+            )
+          }
+          else{
+            const serviceID = 'default_service';
+            const templateID = 'template_o6dhata';
+              emailjs.sendForm(serviceID, templateID, this)
+              .then(() => {
+                
+                
+                names.value='';
+                email.value='';
+                subject.value='';
+                message.value='';
+                Swal.fire(
+                  {
+                    title:'SUCCESS',
+                    text:'Your message has been sent',
+                    icon:'success',
+                    background:'rgba(30,30,30,1)',
+                    color:'#fff',
+                  }
+                )
+              }, (err) => {
+                
+                email.value='';
+                subject.value='';
+                message.value='';
+                Swal.fire(
+                  {
+                    title:'ERROR',
+                    text:'Error: ' + err,
+                    icon:'error',
+                    background:'rgba(30,30,30,1)',
+                    color:'#fff',
+                  }
+                )
+              });
+          }
+        }
+     }
+   }
+   
+ 
+  
 });
